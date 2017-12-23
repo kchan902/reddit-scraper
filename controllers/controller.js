22d97e6cc8bf5e1820b9030027fd8965944daf5b
Karina Chan <karinachan902@gmail.com>
2017-12-22 18:49:59 -0800

    fixed controllers

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,8 +2,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var request = require('request'); 
-var cheerio = require('cheerio'); 
+var request = require('request');
+var cheerio = require('cheerio');
 var axios = require('axios');
 
 // Import the Comment and Article models
@@ -11,10 +11,10 @@ var Comment = require('../models/Comment.js');
 var Article = require('../models/Article.js');
 
 // Index Page Render (first visit to the site)
-router.get('/', function (req, res){
+router.get('/', function(req, res) {
 
-  // Scrape data
-  res.redirect('/scrape');
+    // Scrape data
+    res.redirect('/scrape');
 
 });
 
@@ -24,27 +24,27 @@ var app = express();
 
 
 // Articles Page Render
-router.get('/articles', function (req, res){
+router.get('/articles', function(req, res) {
 
-  // Query MongoDB for all article entries (sort newest to top, assuming Ids increment)
-  Article.find().sort({_id: -1})
+    // Query MongoDB for all article entries (sort newest to top, assuming Ids increment)
+    Article.find().sort({ _id: -1 })
 
-    // But also populate all of the comments associated with the articles.
-    .populate('comments')
+        // But also populate all of the comments associated with the articles.
+        .populate('comments')
 
-    // Then, send them to the handlebars template to be rendered
-    .exec(function(err, doc){
-      // log any errors
-      if (err){
-        console.log(err);
-      } 
-      // or send the doc to the browser as a json object
-      else {
-        var hbsObject = {articles: doc}
-        res.render('index', hbsObject);
-        // res.json(hbsObject)
-      }
-    });
+        // Then, send them to the handlebars template to be rendered
+        .exec(function(err, doc) {
+            // log any errors
+            if (err) {
+                console.log(err);
+            }
+            // or send the doc to the browser as a json object
+            else {
+                var hbsObject = { articles: doc }
+                res.render('index', hbsObject);
+                // res.json(hbsObject)
+            }
+        });
 
 });
 
@@ -52,41 +52,59 @@ router.get('/articles', function (req, res){
 // Web Scrape Route
 router.get('/scrape', function(req, res) {
 
-  // First, grab the body of the html with request
-  axios.get("http://www.reddit.com/").then(function(response) {
+    // First, grab the body of the html with request
+    axios.get("http://www.reddit.com/").then(function(response) {
+        var html = response.data;
 
-    // Then, load html into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(html);
+        // Then, load html into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
 
-    // Store data
-    var results = {};
+        // Store data
+        var results = {};
 
-    // Now, grab every everything with a class of "inner" with each "article" tag
-    $('p.title').each(function(i, element) {
+        var promises = [];
 
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
+        // Now, grab every everything with a class of "inner" with each "article" tag
+        $('p.title').each(function(i, element) {
 
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
-    // Save these results in an object that we'll push into the results array we defined earlier
-      results.title = title
-      results.link = link
-      console.log("1", results)
+            var promise = new Promise(function(resolve, reject) {
+              // Save the text of the element in a "title" variable
+              var title = $(element).text();
 
-      db.Article
-        .create(results)
-        .then(function(dbArticle) {
-          // If we were able to successfully scrape and save an Article, send a message to the client
-          res.send("Scrape Stuff");
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          res.json(err);
+              // In the currently selected element, look at its child elements (i.e., its a-tags),
+              // then save the values for any "href" attributes that the child elements may have
+              var link = $(element).children().attr("href");
+              // Save these results in an object that we'll push into the results array we defined earlier
+              results.title = title
+              results.link = link
+
+              db.Article
+                .create(results)
+                .then(function(dbArticle) {
+                    // If we were able to successfully scrape and save an Article, send a message to the client
+                    // res.send("Scrape Stuff");
+                    resolve(dbArticle);
+                })
+                .catch(function(err) {
+                    // If an error occurred, send it to the client
+                    reject();
+                    // res.json(err);
+                });
+
+            });
+            promises.push(promise);
+
         });
-    });
 
+        Promise.all(promises).then(function(results) {
+          console.log("uhh....");
+          res.send("Scrape stuff");
+
+        });
+
+    })
+    .catch(function(err) {
+      console.error("ERROR!: " + err);
     });
 });
 
@@ -125,48 +143,48 @@ router.get('/scrape', function(req, res) {
 // });
 
 // Add a Comment Route - **API**
-router.post('/add/comment/:id', function (req, res){
+router.post('/add/comment/:id', function(req, res) {
 
-  // Collect article id
-  var articleId = req.params.id;
-  
-  // Collect Author Name
-  var commentAuthor = req.body.name;
+    // Collect article id
+    var articleId = req.params.id;
 
-  // Collect Comment Content
-  var commentContent = req.body.comment;
+    // Collect Author Name
+    var commentAuthor = req.body.name;
 
-  // "result" object has the exact same key-value pairs of the "Comment" model
-  var result = {
-    author: commentAuthor,
-    content: commentContent
-  };
+    // Collect Comment Content
+    var commentContent = req.body.comment;
 
-  // Using the Comment model, create a new comment entry
-  var entry = new Comment (result);
+    // "result" object has the exact same key-value pairs of the "Comment" model
+    var result = {
+        author: commentAuthor,
+        content: commentContent
+    };
 
-  // Save the entry to the database
-  entry.save(function(err, doc) {
-    // log any errors
-    if (err) {
-      console.log(err);
-    } 
-    // Or, relate the comment to the article
-    else {
-      // Push the new Comment to the list of comments in the article
-      Article.findOneAndUpdate({'_id': articleId}, {$push: {'comments':doc._id}}, {new: true})
-      // execute the above query
-      .exec(function(err, doc){
+    // Using the Comment model, create a new comment entry
+    var entry = new Comment(result);
+
+    // Save the entry to the database
+    entry.save(function(err, doc) {
         // log any errors
-        if (err){
-          console.log(err);
-        } else {
-          // Send Success Header
-          res.sendStatus(200);
+        if (err) {
+            console.log(err);
         }
-      });
-    }
-  });
+        // Or, relate the comment to the article
+        else {
+            // Push the new Comment to the list of comments in the article
+            Article.findOneAndUpdate({ '_id': articleId }, { $push: { 'comments': doc._id } }, { new: true })
+                // execute the above query
+                .exec(function(err, doc) {
+                    // log any errors
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        // Send Success Header
+                        res.sendStatus(200);
+                    }
+                });
+        }
+    });
 
 });
 
@@ -174,23 +192,22 @@ router.post('/add/comment/:id', function (req, res){
 
 
 // Delete a Comment Route
-router.post('/remove/comment/:id', function (req, res){
+router.post('/remove/comment/:id', function(req, res) {
 
-  // Collect comment id
-  var commentId = req.params.id;
+    // Collect comment id
+    var commentId = req.params.id;
 
-  // Find and Delete the Comment using the Id
-  Comment.findByIdAndRemove(commentId, function (err, todo) {  
-    
-    if (err) {
-      console.log(err);
-    } 
-    else {
-      // Send Success Header
-      res.sendStatus(200);
-    }
+    // Find and Delete the Comment using the Id
+    Comment.findByIdAndRemove(commentId, function(err, todo) {
 
-  });
+        if (err) {
+            console.log(err);
+        } else {
+            // Send Success Header
+            res.sendStatus(200);
+        }
+
+    });
 
 });
 
